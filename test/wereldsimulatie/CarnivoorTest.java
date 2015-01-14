d929e1fe9e3348b3d32363b63e50eec3aaf32849
takcy1980 <takcy1980@gmail.com>
2015-01-14 13:27:17 +0100

    4 Unit testen voor Eet mehode gemaakt

--- test/wereldsimulatie/CarnivoorTest.java
+++ test/wereldsimulatie/CarnivoorTest.java
@@ -23,6 +23,8 @@ public class CarnivoorTest {
     
     public CarnivoorTest() {
         
+        b = new Herbivoor(pos);
+        
     }
     
     @BeforeClass
@@ -42,21 +44,69 @@ public class CarnivoorTest {
     }
 
     /**
+     * Unit test
      * Test of eet method, of class Carnivoor.
+     * Eatable portion is 500, other b has also enough energy to provide
      */
     @Test
     public void testEet() {
         System.out.println("eet");
-        b = new Herbivoor(pos);
-        int y = b.getEnergie();
-        b.setEnergie(-300);
-        
         Carnivoor instance = new Carnivoor(pos);
+        instance.setEnergie(-1000); 
+        instance.eet(b);
+
+        assertTrue(b.getEnergie() ==  2500);
+
+    }
+    /**
+     * Unit test 
+     * Carnivoor energie is set to 4000, his hunger is 1000, he can eat 500, but animal only has 300
+     * Energy of Herbivoor will be set to 300. Maximum eatable portion is 300, is true when energie is 0
+     */
+        @Test
+    public void testEet2() {
+        System.out.println("eet");
+        Carnivoor instance = new Carnivoor(pos);
+        instance.setEnergie(-1000);
+        b.setEnergie(-2700);
+       
         instance.eet(b);
         int x = b.getEnergie();
-        assertTrue(y - x == 300);
+        assertTrue(b.getEnergie() == 0);
         // TODO review the generated test code and remove the default call to fail.
-        //
+        //    
     }
+    /**
+     * Unit test
+     * Available hunger Carnivoor is 150, other b has more than this to provide
+     */
+        @Test
+    public void testEet3() {
+        System.out.println("eet");
+        Carnivoor instance = new Carnivoor(pos);
+        instance.setEnergie(-150);
+               
+        instance.eet(b);
+        int x = b.getEnergie();
+        assertTrue(b.getEnergie() == 2850);
+ 
+    }
+    
+    /**
+     * Available hunger Carnivoor is 300, other b has only 200 to provide
+     */
+        @Test
+    public void testEet4() {
+        System.out.println("eet");
+        Carnivoor instance = new Carnivoor(pos);
+        instance.setEnergie(-300);
+        b.setEnergie(-2800);
+               
+        instance.eet(b);
+        int x = b.getEnergie();
+        assertTrue(b.getEnergie() == 0);
+ 
+    }
+
     
 }
