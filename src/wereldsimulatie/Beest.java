f8127004e9ad9edaa3f6ef21ac10ad2853fda4bd
Erazor1771 <lars_ke@hotmail.com>
2015-01-19 13:00:42 +0100

    Missende getters + setters voor methode paar en methode paar gefixed (version 0.1)

--- src/wereldsimulatie/Beest.java
+++ src/wereldsimulatie/Beest.java
@@ -124,36 +124,25 @@ abstract public class Beest<T> extends Observable implements Serializable {
         return this.energie;
     }
 
-    /**
-     * @author Lars Maak één (1) nieuw Beest. Het nieuwe beest erft
+     /**
+     * Maak één (1) nieuw Beest. Het nieuwe beest erft
      * eigenschappen van ouders:<br>
      * Digestie, Stamina, Legs, Hitsigheid, Voortplantingskosten, Strength,
-     * Zwemdrempel, Beweegdrempel <br>
+     * Zwemdrempel, Beweegdrempel, Energie <br>
      * Eigenschappen van het kind zijn het gemiddelde van bovenstaande waarden
      * +/- random waarde tussen 0 en 10% van het verschil van de betreffende
      * waarde van de ouders.<br>
      * Een beest is bereid om te paren vanaf 60% van zijn stamina. Dit punt
      * wordt ook wel zijn hitsigheid genoemd. Dat paren vindt echter alleen
      * plaats als het andere beest (zie argument) dat punt van hitsigheid ook
-     * bereikt heeft. <br>
-     * Als verschillende soorten beesten paren winnen de dominante genen, de
-     * volgorde van dominante genen bij beesten:<br>
-     * 1. Omnivoren <br>
-     * 2. Carnivoren<br>
-     * 3. Herbivoren<br>
+     * bereikt heeft. Dit wordt in klasse Eiland bepaald door methode isHitsig() <br>
+     * Als 2 verschillende soorten beesten paren 50% kans op één van de twee type beesten:<br>
+     * Bij 2 dezelfde soort beesten 100% kans bij paren op dat type beest.
      * @return Beest (kind)
      * @param b Beest waarmee gepaard wordt
-     *
-     * @TODO Als verschillende soorten beesten paren, winnen de dominante genen
-     * moet veranderd worden.(Oplossing: 50/50 type beesten die paren)
      */
-    // @TODO ?Digestie bepalen
-    // @TODO ?Hitsigheid bepalen
-    // @TODO ?Energie bepalen en veranderen 
-    // @TODO ? ADDED: Dit beest haalt van ander beest af bij bepalen marge
+    
     public Beest paar(Beest b) {
-        // Als beide dieren hitsigheid boven 60 hebben ga dan paren
-
         // Trek voortplantingskosten ouders af
         this.kostenStaminaBeest();
         b.kostenStaminaBeest();
@@ -161,55 +150,70 @@ abstract public class Beest<T> extends Observable implements Serializable {
         // Bepaal de values van een beest
         // Gemiddelde bepalen en deze casten naar een int
         int gemiddelde_strength = (int) (this.getStrength() + b.getStrength()) / 2;
-        //int gemiddelde_stamina = (int) (this.getStamina() + b.getStamina()) / 2;
-        //int gemiddelde_energie = (int) (this.getEnergie() + b.getEnergie()) / 2;
+        int gemiddelde_stamina = (int) (this.getStamina() + b.getStamina()) / 2;
+        int gemiddelde_energie = (int) (this.getEnergie() + b.getEnergie()) / 2;
         int gemiddelde_legs = (int) (this.getLegs() + b.getLegs()) / 2;
- //       int gemiddelde_gewicht = (int) (this.getGewicht() + b.getGewicht()) / 2;
+        int gemiddelde_gewicht = (int) (this.getGewicht() + b.getGewicht()) / 2;
+        int gemiddelde_hitsigheid = (int) (this.getHitsigheid() + b.getHitsigheid()) / 2;
+        int gemiddelde_voortplantingskosten = (int) (this.getVoortplantingsKosten() + b.getVoortplantingsKosten()) / 2;
+        int gemiddelde_zwemdrempel = (int) (this.getZwemDrempel() + b.getZwemDrempel()) / 2;
+        int gemiddelde_beweegdrempel = (int) (this.getBeweegDrempel() + b.getBeweegDrempel()) / 2;
         // Verschil ouders berekenen
         int verschil_strength = this.getStrength() - b.getStrength();
-        //int verschil_stamina = this.getStamina() - b.getStamina();
-        //int verschil_energie = this.getEnergie() - b.getEnergie();
+        int verschil_stamina = this.getStamina() - b.getStamina();
+        int verschil_energie = this.getEnergie() - b.getEnergie();
         int verschil_legs = this.getLegs() - b.getLegs();
-  //      int verschil_gewicht = this.getGewicht() - b.getGewicht();
-        // Percentage Bepalen
-        double percentage_strength = (double) (Math.random() * 10) / 100;
-        //double percentage_stamina = (double) (Math.random() * 10) / 100;
-        //double percentage_energie = (double) (Math.random() * 10) / 100;
-        double percentage_legs = (double) (Math.random() * 10) / 100;
-  //      double percentage_gewicht = (double) (Math.random() * 10) / 100;
+        int verschil_gewicht = this.getGewicht() - b.getGewicht();
+        int verschil_hitsigheid = this.getHitsigheid() - b.getHitsigheid();
+        int verschil_voortplantingskosten = this.getVoortplantingsKosten() - b.getVoortplantingsKosten();
+        int verschil_zwemdrempel = this.getZwemDrempel() - b.getZwemDrempel();
+        int verschil_beweegdrempel = this.getBeweegDrempel() - b.getBeweegDrempel();
+        
+        // Percentage Bepalen tussen 0 en 10%
+        double percentage =  (Math.random() * 10) / 100 ;
+        
         // Zet percentage double om in Integer
         // Marge wat kind van de ouders nog overerft
-        double marge_strengthTemp = (double)verschil_strength * percentage_strength;
-        int marge_strength = (int)marge_strengthTemp;
+        int marge_strength = verschil_strength * (int) percentage;
+        int marge_stamina = verschil_stamina * (int) percentage;
+        int marge_energie = verschil_energie * (int) percentage;
+        int marge_legs = verschil_legs * (int) percentage;
+        int marge_gewicht = verschil_gewicht * (int) percentage;
+        int marge_histigheid = verschil_hitsigheid * (int) percentage;
+        int marge_voortplantingskosten = verschil_voortplantingskosten * (int) percentage;
+        int marge_zwemdrempel = verschil_zwemdrempel * (int) percentage;
+        int marge_beweegdrempel = verschil_beweegdrempel * (int) percentage;
         
-        //int marge_stamina = verschil_stamina * (int) percentage_stamina;
-        //int marge_energie = verschil_energie * (int) percentage_energie;
-        double marge_legsTemp = (double)verschil_legs * percentage_legs;
-        int marge_legs = (int)marge_legsTemp;
-        
-        //int marge_gewicht = verschil_gewicht * (int) percentage_gewicht;
-
-        int strengthTemp = 0;
-        int legsTemp = 0;
-
+        System.out.println("Gemiddelde strength: " + gemiddelde_strength);
+        System.out.println("Verschil strength: " + verschil_strength);
+        System.out.println("Percentage: " + percentage);
+        System.out.println("Marge strength: " + marge_strength);
         // Random met een kans van 50 / 50
         Random random = new Random();
         int num = random.nextInt(2);
         // +
         if (num == 0) {
-            strengthTemp = gemiddelde_strength + marge_strength;
-            //stamina = gemiddelde_stamina + marge_stamina;
-            //energie = gemiddelde_energie + marge_energie;
-            legsTemp = gemiddelde_legs + marge_legs;
-   //         gewichtTemp = gemiddelde_gewicht + marge_gewicht;
-
+            strength = gemiddelde_strength + marge_strength;
+            stamina = gemiddelde_stamina + marge_stamina;
+            energie = gemiddelde_energie + marge_energie;
+            legs = gemiddelde_legs + marge_legs;
+            gewicht = gemiddelde_gewicht + marge_gewicht;
+            hitsigheid = gemiddelde_hitsigheid + marge_histigheid;
+            voortplantingsKosten = gemiddelde_voortplantingskosten + marge_voortplantingskosten;
+            zwemDrempel = gemiddelde_zwemdrempel + marge_zwemdrempel;
+            beweegDrempel = gemiddelde_beweegdrempel + marge_beweegdrempel;
+            
         } // -
         else {
-            strengthTemp = gemiddelde_strength - marge_strength;
-            //stamina = gemiddelde_stamina - marge_stamina;
-            //energie = gemiddelde_energie - marge_energie;
-            legsTemp = gemiddelde_legs - marge_legs;
-  //          gewichtTemp = gemiddelde_gewicht - marge_gewicht;
+             strength = gemiddelde_strength - marge_strength;
+            stamina = gemiddelde_stamina - marge_stamina;
+            energie = gemiddelde_energie - marge_energie;
+            legs = gemiddelde_legs - marge_legs;
+            gewicht = gemiddelde_gewicht - marge_gewicht;
+            hitsigheid = gemiddelde_hitsigheid - marge_histigheid;
+            voortplantingsKosten = gemiddelde_voortplantingskosten - marge_voortplantingskosten;
+            zwemDrempel = gemiddelde_zwemdrempel - marge_zwemdrempel;
+            beweegDrempel = gemiddelde_beweegdrempel - marge_beweegdrempel;
         }
 
         // Als beide beesten omnivoor zijn doe dan dit ook uitvoeren                                      
@@ -217,31 +221,49 @@ abstract public class Beest<T> extends Observable implements Serializable {
             // @TODO Positie bepalen
             // Maak Omnivoor
             Omnivoor o = new Omnivoor(this.getPositie());
-            o.setStrength(strengthTemp);
-            o.legs = legsTemp;
-            int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-            o.setEnergie(overgevenVoortplantingsEnergie);
-
+            o.setEnergie(voortplantingsKosten);
+            o.setStrength(strength);
+            o.setStamina(stamina);
+            o.setBeweegDrempel(beweegDrempel);
+            o.setHitsigheid(hitsigheid);
+            o.setLegs(legs);
+            o.setVoortplantingsKosten(voortplantingsKosten);
+            o.setZwemDrempel(zwemDrempel);
+            o.setGewicht(gewicht);
+            
+            System.out.println(o.toString());
             return o;
         } // Als beide beesten carnivoor zijn doe dan dit ook uitvoeren                                      
         else if (this instanceof Carnivoor && b instanceof Carnivoor) {
             // @TODO Positie bepalen
             // Maak Omnivoor
             Carnivoor c = new Carnivoor(this.getPositie());
-            c.setStrength(strengthTemp);
-            c.legs = legsTemp;
-            int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-            c.setEnergie(overgevenVoortplantingsEnergie);
+            c.setEnergie(voortplantingsKosten);
+            c.setStrength(strength);
+            c.setStamina(stamina);
+            c.setBeweegDrempel(beweegDrempel);
+            c.setHitsigheid(hitsigheid);
+            c.setLegs(legs);
+            c.setVoortplantingsKosten(voortplantingsKosten);
+            c.setZwemDrempel(zwemDrempel);
+            c.setGewicht(gewicht);
+            System.out.println(c.toString());
             return c;
         } // Als beide beesten herbivoor zijn doe dan dit ook uitvoeren                                      
         else if (this instanceof Herbivoor && b instanceof Herbivoor) {
             // @TODO Positie bepalen
             // Maak Omnivoor
             Herbivoor h = new Herbivoor(this.getPositie());
-            h.setStrength(strengthTemp);
-            h.legs = legsTemp;
-            int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-            h.setEnergie(overgevenVoortplantingsEnergie);
+            h.setEnergie(voortplantingsKosten);
+            h.setStrength(strength);
+            h.setStamina(stamina);
+            h.setBeweegDrempel(beweegDrempel);
+            h.setHitsigheid(hitsigheid);
+            h.setLegs(legs);
+            h.setVoortplantingsKosten(voortplantingsKosten);
+            h.setZwemDrempel(zwemDrempel);
+            h.setGewicht(gewicht);
+            System.out.println(h.toString());
             return h;
         } // Omnivoor en carnivoor dan dit uitvoeren
         else if ((this instanceof Omnivoor && b instanceof Carnivoor)
@@ -253,19 +275,31 @@ abstract public class Beest<T> extends Observable implements Serializable {
             if (rbeest == 0) {
                 // Maak Omnivoor
                 Omnivoor o = new Omnivoor(this.getPositie());
-                o.setStrength(strengthTemp);
-                o.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                o.setEnergie(overgevenVoortplantingsEnergie);
+                o.setEnergie(voortplantingsKosten);
+                o.setStrength(strength);
+                o.setStamina(stamina);
+                o.setBeweegDrempel(beweegDrempel);
+                o.setHitsigheid(hitsigheid);
+                o.setLegs(legs);
+                o.setVoortplantingsKosten(voortplantingsKosten);
+                o.setZwemDrempel(zwemDrempel);
+                o.setGewicht(gewicht);
+                System.out.println(o.toString());
                 return o;
             }
             if (rbeest == 1) {
                 // Maak Carnivoor
                 Carnivoor c = new Carnivoor(this.getPositie());
-                c.setStrength(strengthTemp);
-                c.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                c.setEnergie(overgevenVoortplantingsEnergie);
+                c.setEnergie(voortplantingsKosten);
+                c.setStrength(strength);
+                c.setStamina(stamina);
+                c.setBeweegDrempel(beweegDrempel);
+                c.setHitsigheid(hitsigheid);
+                c.setLegs(legs);
+                c.setVoortplantingsKosten(voortplantingsKosten);
+                c.setZwemDrempel(zwemDrempel);
+                c.setGewicht(gewicht);
+                System.out.println(c.toString());
                 return c;
             }
         } // Omnivoor en herbivoor dan dit uitvoeren
@@ -278,19 +312,31 @@ abstract public class Beest<T> extends Observable implements Serializable {
             if (rbeest == 0) {
                 // Maak Omnivoor
                 Omnivoor o = new Omnivoor(this.getPositie());
-                o.setStrength(strengthTemp);
-                o.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                o.setEnergie(overgevenVoortplantingsEnergie);
+                o.setEnergie(voortplantingsKosten);
+                o.setStrength(strength);
+                o.setStamina(stamina);
+                o.setBeweegDrempel(beweegDrempel);
+                o.setHitsigheid(hitsigheid);
+                o.setLegs(legs);
+                o.setVoortplantingsKosten(voortplantingsKosten);
+                o.setZwemDrempel(zwemDrempel);
+                o.setGewicht(gewicht);
+                System.out.println(o.toString());
                 return o;
             }
             if (rbeest == 1) {
                 // Maak Herbivoor
                 Herbivoor h = new Herbivoor(this.getPositie());
-                h.setStrength(strengthTemp);
-                h.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                h.setEnergie(overgevenVoortplantingsEnergie);
+                h.setEnergie(voortplantingsKosten);
+                h.setStrength(strength);
+                h.setStamina(stamina);
+                h.setBeweegDrempel(beweegDrempel);
+                h.setHitsigheid(hitsigheid);
+                h.setLegs(legs);
+                h.setVoortplantingsKosten(voortplantingsKosten);
+                h.setZwemDrempel(zwemDrempel);
+                h.setGewicht(gewicht);
+                System.out.println(h.toString());
                 return h;
             }
         } // Carnivoor en herbivoor dan dit uitvoeren
@@ -303,19 +349,31 @@ abstract public class Beest<T> extends Observable implements Serializable {
             if (rbeest == 0) {
                 // Maak Omnivoor
                 Omnivoor o = new Omnivoor(this.getPositie());
-                o.setStrength(strengthTemp);
-                o.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                o.setEnergie(overgevenVoortplantingsEnergie);
+                o.setEnergie(voortplantingsKosten);
+                o.setStrength(strength);
+                o.setStamina(stamina);
+                o.setBeweegDrempel(beweegDrempel);
+                o.setHitsigheid(hitsigheid);
+                o.setLegs(legs);
+                o.setVoortplantingsKosten(voortplantingsKosten);
+                o.setZwemDrempel(zwemDrempel);
+                o.setGewicht(gewicht);
+                System.out.println(o.toString());
                 return o;
             }
             if (rbeest == 1) {
                 // Maak Herbivoor
                 Herbivoor h = new Herbivoor(this.getPositie());
-                h.setStrength(strengthTemp);
-                h.legs = legsTemp;
-                int overgevenVoortplantingsEnergie = this.voortplantingsKosten + b.voortplantingsKosten;
-                h.setEnergie(overgevenVoortplantingsEnergie);
+                h.setEnergie(voortplantingsKosten);
+                h.setStrength(strength);
+                h.setStamina(stamina);
+                h.setBeweegDrempel(beweegDrempel);
+                h.setHitsigheid(hitsigheid);
+                h.setLegs(legs);
+                h.setVoortplantingsKosten(voortplantingsKosten);
+                h.setZwemDrempel(zwemDrempel);
+                h.setGewicht(gewicht);
+                System.out.println(h.toString());
                 return h;
             }
         }
@@ -496,5 +554,61 @@ abstract public class Beest<T> extends Observable implements Serializable {
     public void setStrength(int strength) {
         this.strength = strength;
     }
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+        setChanged();
+        notifyObservers(); 
+    }
 
+    public void setLegs(int legs) {
+        this.legs = legs;
+        setChanged();
+        notifyObservers(); 
+    }
+
+    public void setHitsigheid(int hitsigheid) {
+        this.hitsigheid = hitsigheid;
+        setChanged();
+        notifyObservers(); 
+    }
+
+    public void setZwemDrempel(int zwemDrempel) {
+        this.zwemDrempel = zwemDrempel;
+        setChanged();
+        notifyObservers(); 
+    }
+
+    public void setVoortplantingsKosten(int voortplantingsKosten) {
+        this.voortplantingsKosten = voortplantingsKosten;
+        setChanged();
+        notifyObservers(); 
+    }
+
+    public void setBeweegDrempel(int beweegDrempel) {
+        this.beweegDrempel = beweegDrempel;
+        setChanged();
+        notifyObservers(); 
+    }
+
+    public void setGewicht(int gewicht) {
+        this.gewicht = gewicht;
+        setChanged();
+        notifyObservers(); 
+    }
+    
+    public int getHitsigheid() {
+        return hitsigheid;
+    }
+
+    public int getZwemDrempel() {
+        return zwemDrempel;
+    }
+
+    public int getBeweegDrempel() {
+        return beweegDrempel;
+    }
+
+    public int getVoortplantingsKosten() {
+        return voortplantingsKosten;
+    }
 }
